530a40d338736c0247517a1f651cce46c3ae28e1
ofraam <oamir@seas.harvard.edu>
2018-03-05 21:28:25 +0200

    starting to implement dynamics analysis

--- replay.py
+++ replay.py
@@ -754,6 +754,139 @@ def check_participant_answer(userid):
                         return 'solvedCorrect'
         return 'wrong'
 
+
+def moves_stats(output_file):
+    moves_data_matrics = {}
+    data_first_moves = {}
+
+    first_moves_data_matrices = {}
+    for g in range(len(LOGFILE)):
+        # print g
+        initial_board = copy.deepcopy(START_POSITION[g])
+
+        path_counter = 0.0
+        path_counter_subpaths = 0.0
+        taken_cells = 0.0
+
+        for i in range(len(initial_board)):
+            for j in range(len(initial_board[i])):
+                if ((initial_board[i][j]!=1) & (initial_board[i][j]!=2)):
+                    initial_board[i][j] = int(initial_board[i][j])
+                elif (initial_board[i][j]==1):
+                    initial_board[i][j]='X'
+                    taken_cells+=1
+                elif (initial_board[i][j]==2):
+                    initial_board[i][j]='O'
+                    taken_cells+=1
+
+        move_matrix = copy.deepcopy(initial_board)
+        first_move_matrix = copy.deepcopy(initial_board)
+
+        curr_move_matrix = copy.deepcopy(move_matrix)
+        curr_first_move_matrix = copy.deepcopy(move_matrix)
+
+
+        results_table = []
+        with open(LOGFILE[g], 'rb') as csvfile:
+            print LOGFILE[g]
+            condition = LOGFILE[g][5:-10].replace("_",",")
+            board_name = LOGFILE[g]
+            board_name=board_name[:-4]
+            log_reader = csv.DictReader(csvfile)
+            move_number = 1
+            move_stack = []
+            curr_path = []
+            curr_user = ''
+            prev_time = None
+            prev_action = None
+            for row in log_reader:
+                curr_data = {}
+                if curr_user == '':
+                    curr_user = row['userid']
+                    participant_answer = check_participant_answer(curr_user)
+
+                if row['userid'] != curr_user:
+                    participant_answer = check_participant_answer(curr_user)
+
+                    # reset all values for next user
+                    prev_time = None
+                    prev_action = None
+                    prev_row = None
+                    first_move_for_player = True
+                    move_number = 1
+                    curr_path = []
+                    move_stack = []
+
+                elif row['key'] == 'clickPos':
+                    rowPos = int(row['value'][0])
+                    colPos = int(row['value'][2])
+                    move_stack.append((rowPos, colPos))
+                    player = int(row['value'][4])
+                    first_move = False
+                    # print rowPos
+                    # print colPos
+                    # print move_matrix[rowPos][colPos]
+                    if (curr_move_matrix[rowPos][colPos]!='X') & (curr_move_matrix[rowPos][colPos]!='O'):
+                        curr_move_matrix[rowPos][colPos] = player
+                        curr_path.append([rowPos, colPos, player])
+
+                        if len(curr_path) == 1:
+                            first_move = True
+                    time_between = 0
+                    if prev_time == None:
+                        if prev_row != None:
+                            # print 'why'
+                            prev_time = prev_row['time']
+                    if prev_time == None:
+                        print row
+                    time_between = int(row['time']) - int(prev_time)
+                    time_between = time_between/1000.0
+
+                    prev_action = 'click'
+                    prev_time = row['time']
+
+                    curr_data['userid'] = curr_user
+                    curr_data['condition'] = condition
+                    curr_data['board_name'] = board_name
+                    curr_data['solved'] = participant_answer
+                    curr_data['player'] = player
+                    curr_data['position'] = str(rowPos) + '_' + str(colPos)
+                    curr_data['time'] = row['time']
+                    curr_data['time_prev_move'] = prev_time
+                    curr_data['time_between'] = time_between
+                    curr_data['prev_action'] = prev_action
+                    curr_data['first_move_in_path'] = first_move
+                    curr_data['move_number'] = move_number
+                    results_table.append(curr_data)
+                    move_number += 1
+
+                elif row['key'] == 'reset':
+                    curr_move_matrix = copy.deepcopy(initial_board)
+                    prev_action = 'reset'
+                    prev_time = row['time']
+
+                elif row['key'] == 'undo':
+                    if len(move_stack) > 0:
+                        undo_move = move_stack.pop()
+                        curr_move_matrix[undo_move[0]][undo_move[1]] = 0
+                    else:
+                        print 'problem undo'
+
+                    prev_action = 'undo'
+                    prev_time = row['time']
+                # else:
+                #     prev_time = row['time']
+
+                prev_row = copy.deepcopy(row)
+
+    dataWriter = csv.DictWriter(output_file, fieldnames=results_table[0].keys(), delimiter=',')
+    dataWriter.writeheader()
+    for record in results_table:
+        dataWriter.writerow(record)
+
+
+
+
 def paths_stats(participants = 'all'):
     moves_data_matrics = {}
     data_first_moves = {}
@@ -915,7 +1048,7 @@ def paths_stats(participants = 'all'):
 
 
 
-                elif row['key'] == 'clickPos':
+                if row['key'] == 'clickPos':
                     rowPos = int(row['value'][0])
                     colPos = int(row['value'][2])
                     player = int(row['value'][4])
@@ -1582,7 +1715,8 @@ if __name__ == "__main__":
     # paths_stats(participants='all')
     # paths_stats(participants='solvedCorrect')
     # paths_stats(participants='wrong')
-    paths_stats(participants='wrong')
+    # paths_stats(participants='wrong')
+    moves_stats('stats/testMovesStats.csv')
     # seperate_log('logs/fullLogCogSci.csv')
     # # entropy_board()
     # # entropy_board(ignore=True)
